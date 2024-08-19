0a767786440011003b8b799c832ef7c5d80e18e8
IamSpotted <jon.uldrick@iamspotted.com>
2024-08-18 23:28:37 -0400

    Update ChatgptScoutAI.py

--- ChatgptScoutAI.py
+++ ChatgptScoutAI.py
@@ -1,19 +1,20 @@
 import os
 import warnings
 from langchain.chains import RetrievalQA
-from langchain.llms import HuggingFacePipeline
-from langchain.vectorstores import Chroma
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.llms import HuggingFacePipeline
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from get_embedding_function import get_embedding_function
 
 
 #Ignore Warnings
-warnings.filterwarnings("ignore")
+#warnings.filterwarnings("ignore")
 
 #Set Chroma Path
 CHROMA_PATH = "/home/cavllm/ScoutAI/database"
 
 #Load Embeddings
-embedding_function = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+embedding_function = get_embedding_function()
 
 #Initialize ChromaDB
 vector_store = Chroma(
@@ -25,7 +26,7 @@ vector_store = Chroma(
 retriever = vector_store.as_retriever()
 
 #Intialize HuggingFace model
-chat = HuggingFacePipeline(model="Ollama/phi3:mini", task="text_generation")
+chat = HuggingFacePipeline(model="sentence-transformers/all-MiniLM-L6-v2", task="text_generation")
 
 #Setup RetrievalQA Chain
 qa = RetrievalQA.from_chain_type(
